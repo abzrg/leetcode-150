3f0189fcd3323dcf238d9fd5ffe24810c89e80c2
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2024-06-25 18:17:57 +0330

    make/common: Updated the CXXFLAGS
    Now it checks for OS to figure out which flags to set

--- make/common.mk
+++ make/common.mk
@@ -1,15 +1,30 @@
 MAKEFLAGS += --warn-undefined-variables
 
 CXX = clang++
+
 CXXFLAGS = -Wall -Wextra -std=c++20 -pedantic
-CXXFLAGS += -gdwarf-4 -ggdb3 -O0
 CXXFLAGS += -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare \
 			-Wno-unused-parameter -Wno-unused-variable \
 			-Wno-unused-but-set-variable -Wshadow
+
 ifeq ($(CXX), clang++)
 CXXFLAGS  += -ferror-limit=1
 endif
 
+CXXFLAGS += -O0
+OS := $(shell uname)
+ifeq ($(OS), Linux)
+CXXFLAGS += -gdwarf-4 -ggdb3
 LDLIBS = -lm
+else ifeq ($(OS), Darwin)
+CXXFLAGS += -g
+LDLIBS = -lm
+else ifeq ($(OS), Windows_NT)
+CXXFLAGS += -g
+else
+# Settings for other systems
+CXXFLAGS += -g
+endif
+
 
 BUILDDIR = build
